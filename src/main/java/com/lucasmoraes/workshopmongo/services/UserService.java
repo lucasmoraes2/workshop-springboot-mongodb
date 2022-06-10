acd7c03a12d067b8217047c00c4d2454a2d8ac93
Lucas Moraes <lucasmoraes2@outlook.com.br>
2022-06-09 23:59:35 -0300

    Obtendo um usuário por id

--- src/main/java/com/lucasmoraes/workshopmongo/services/UserService.java
+++ src/main/java/com/lucasmoraes/workshopmongo/services/UserService.java
@@ -2,10 +2,12 @@ package com.lucasmoraes.workshopmongo.services;
 
 import com.lucasmoraes.workshopmongo.domain.User;
 import com.lucasmoraes.workshopmongo.repository.UserRepository;
+import com.lucasmoraes.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,5 +17,10 @@ public class UserService {
 
     public List<User> findAll() {
         return repository.findAll();
+    }
+
+    public User findById(String id) {
+        Optional<User> user = repository.findById(id);
+        return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 }

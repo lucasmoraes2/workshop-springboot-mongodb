e62d36ad20b1f50de91af1e1fe2acc476f59e2cf
Lucas Moraes <lucasmoraes2@outlook.com.br>
2022-06-10 00:42:20 -0300

    Inserção de usuário com POST

--- src/main/java/com/lucasmoraes/workshopmongo/services/UserService.java
+++ src/main/java/com/lucasmoraes/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.lucasmoraes.workshopmongo.services;
 
 import com.lucasmoraes.workshopmongo.domain.User;
+import com.lucasmoraes.workshopmongo.dto.UserDTO;
 import com.lucasmoraes.workshopmongo.repository.UserRepository;
 import com.lucasmoraes.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,7 +21,15 @@ public class UserService {
     }
 
     public User findById(String id) {
-        Optional<User> user = repository.findById(id);
-        return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+        Optional<User> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User obj) {
+        return repository.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto) {
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 }

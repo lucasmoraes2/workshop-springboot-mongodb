7538b18987f1aaaadce335594b13b53cbdfd26a8
Lucas Moraes <lucasmoraes2@outlook.com.br>
2022-06-14 14:22:19 -0300

    Referenciando os posts do usuário

--- src/main/java/com/lucasmoraes/workshopmongo/domain/User.java
+++ src/main/java/com/lucasmoraes/workshopmongo/domain/User.java
@@ -1,7 +1,11 @@
 package com.lucasmoraes.workshopmongo.domain;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Document(collection = "user")
 public class User {
@@ -10,6 +14,9 @@ public class User {
     private String id;
     private String name;
     private String email;
+
+    @DBRef(lazy = true)
+    private List<Post> posts = new ArrayList<>();
 
     public User() {
     }
@@ -42,6 +49,14 @@ public class User {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
     }
 
     @Override

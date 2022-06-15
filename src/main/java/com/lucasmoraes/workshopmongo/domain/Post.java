be941a34c064486d97f4143af72ec0362df92d45
Lucas Moraes <lucasmoraes2@outlook.com.br>
2022-06-15 00:27:02 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/lucasmoraes/workshopmongo/domain/Post.java
+++ src/main/java/com/lucasmoraes/workshopmongo/domain/Post.java
@@ -1,10 +1,13 @@
 package com.lucasmoraes.workshopmongo.domain;
 
 import com.lucasmoraes.workshopmongo.dto.AuthorDTO;
+import com.lucasmoraes.workshopmongo.dto.CommentDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Document
 public class Post {
@@ -15,6 +18,8 @@ public class Post {
     private String title;
     private String body;
     private AuthorDTO author;
+
+    private List<CommentDTO> comments = new ArrayList<>();
 
     public Post() {
     }
@@ -65,6 +70,14 @@ public class Post {
 
     public void setAuthor(AuthorDTO author) {
         this.author = author;
+    }
+
+    public List<CommentDTO> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<CommentDTO> comments) {
+        this.comments = comments;
     }
 
     @Override
